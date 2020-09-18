2486e26353a15eb3e3db0f6d906f9cc9a04961cc
bobihariadi <bobihariadi@gmial.com>
2020-09-18 08:37:44 +0700

    Updates

---
+++ precache-manifest.9234cea6182ebdf43cf6c72d17334c4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802eaac2b045e0eb4d32d61aee4e4327",
+    "url": "/reactjs-my-web-lat/index.html"
+  },
+  {
+    "revision": "74982334e7e218af59cb",
+    "url": "/reactjs-my-web-lat/static/css/main.6fb13ef1.chunk.css"
+  },
+  {
+    "revision": "ef765086e4b52b489dc8",
+    "url": "/reactjs-my-web-lat/static/js/2.429034c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-my-web-lat/static/js/2.429034c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74982334e7e218af59cb",
+    "url": "/reactjs-my-web-lat/static/js/main.32d9ca4b.chunk.js"
+  },
+  {
+    "revision": "c204b00b2bbc89af522d",
+    "url": "/reactjs-my-web-lat/static/js/runtime-main.6f7224f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjs-my-web-lat/static/media/logo.5d5d9eef.svg"
+  }
+]);
